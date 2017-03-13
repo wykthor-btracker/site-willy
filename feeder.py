1e200f28bafbad9827fa4f205f04982ce1d05442
Wykthor <wykthor.g@wgvale.com>
2017-03-13 08:46:54 -0300

    update

--- feeder.py
+++ feeder.py
@@ -22,11 +22,15 @@ class Picture_Parser(HTMLParser):
 			self.div = 1
 		try:
 			for i in attrs:
-				if('src' in i and self.found <2):
+				print(i)
+				if('src' in i and self.found <2 and not '/textinputassistant/tia.png' in i[1]):
 					self.data = i[1]
 					self.found +=1
+			print('\n')
 		except:
 			pass
+	def handle_data(self,data):
+		
 	def handle_endtag(self,tag):
 		if(tag=="div"):
 			self.tag = 0
@@ -48,7 +52,7 @@ class Yt_Url_HTMLParser(HTMLParser):
 			self.att = 0
 	def handle_data(self, data):
 		if(self.div == 1 and self.att == 1 and 'https://www.youtube.com/watch?v=' in data and self.found == 0):
-			self.data = data
+			self.data = data.replace('watch?v=','embed/')
 			self.found = 1
 	def handle_endtag(self,tag):
 		if(tag=="div"):
@@ -68,22 +72,29 @@ def parse_list(filename):
 	
 def get_keys(name,keys):
 	ia = imdb()
-	result = ia.search_movie(name)
-	result = result[0]
-	keys = {'title':str(result['title'].encode('ascii','ignore')),'year':result['year'],'id':result.movieID,'plot':str(ia.get_movie_plot(result.movieID)['data']['plot'][0]).encode('ascii','ignore')}
-	keys['title'] += ' trailer'
+	print(name)
+	try:	
+		result = ia.search_movie(name)
+		result = result[0]
+		keys = {'title':result['title'].encode('utf-8','ignore'),'year':result['year'],'id':result.movieID,'plot':ia.get_movie_plot(result.movieID)['data']['plot'][0].encode('utf-8','ignore')}
+		keys['title'] += ' trailer'	
+		print("OK")
+	except:
+		print("Something went wrong with "+name)
+		keys = {'title':'null','id':'-1','plot':'null','year':'null'}
 	return(keys)
 	
 def get_resource_url(keys,url):
-	url+='+'+'+'.join(keys['title'].split(' '))+'+'+str(keys['year'])
-	return(url)
-def get_poster_url(keys):
-	url = "http://www.google.com/images?q=site:http://www.impawards.com"
+		url+='+'+'+'.join(keys['title'].split(' '))+'+'+str(keys['year'])
+		return(url)
 #functions
 
 #main
 def main(args):
+	errors = 0
+	movies = 0
 	keys = []
+	lists = []
 	names = []
 	name = ''
 	names = parse_list('input.txt')
@@ -92,23 +103,28 @@ def main(args):
 		name = item
 		keys = get_keys(name,keys)
 		url = get_resource_url(keys,url)
-		r = get(url)
-		text = normalize('NFKD', r.text).encode('ascii','ignore')
-		a = Yt_Url_HTMLParser()
-		a.init()
-		a.feed(text)
-		keys['url']=a.spit()
-		url = "http://www.google.com/images?q=site:http://www.impawards.com"
-		url = get_resource_url(keys,url)
-		r = get(url)
-		text = normalize('NFKD', r.text).encode('ascii','ignore')
-		a = Picture_Parser()
-		a.init()
-		a.feed(text)
-		keys['pic'] = a.spit()
-		for item,key in keys.iteritems():
-			print(key,item)		
-		populate_json(keys)
+		if(keys['id']!='-1'):
+			movies+=1
+			r = get(url)
+			text = normalize('NFKD', r.text).encode('ascii','ignore')
+			a = Yt_Url_HTMLParser()
+			a.init()
+			a.feed(text)
+			keys['url']=a.spit()
+			url = "http://www.google.com/images?q=site:http://www.impawards.com"
+			url = get_resource_url(keys,url)
+			r = get(url)
+			text = normalize('NFKD', r.text).encode('ascii','ignore')
+			a = Picture_Parser()
+			a.init()
+			a.feed(text)
+			keys['pic'] = a.spit()
+			lists.append(keys)
+		else:
+			errors+=1
+	populate_json(lists)
+	print("Total movies added:"+str(movies))
+	print("Total errors:"+str(errors))
 	return 0
 #main
 if __name__ == '__main__':
---
+++ genres.py
@@ -0,0 +1,41 @@
+#imports
+from imdb import IMDb as imdb
+import json
+#imports#
+
+#variables
+
+#variables#
+
+#classes
+
+#classes#
+
+#function
+def parse_genres():
+	flist = []
+	ia = imdb()
+	with open('data.json','r') as f:
+		fdict = json.load(f)
+	for i in fdict:
+		info = ia.get_movie(int(i['id']),'main')
+		info = info['genres']
+		for u in info:
+			if(not u.encode('utf-8','ignore') in flist):
+				flist.append()
+	return(flist)
+def populate_genres(flist):
+	with open('genres.txt','w') as f:
+		for i in flist:
+			f.write(i+'\n')
+#function#
+
+#main
+def main(args):
+	flist = parse_genres()
+	print(flist)
+	return 0
+#main#
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
